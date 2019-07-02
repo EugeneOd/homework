2d615b679997434de0c0409049591467d6b14e3b
Eugene Kuchma <evgnkuchma@gmail.com>
2019-07-02 13:03:38 +0300

    HW6 p1

--- home6/part1/js/script.js
+++ home6/part1/js/script.js
@@ -1,7 +1,26 @@
 "use strict"
 
-function isNumeric(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+function home1() {
+    var inputNumbers = prompt('Enter the numbers separated by commas (amount of numbers from 3 to 10)');
+    var numbers = inputNumbers.split(',');
+    
+    if (numbers.length < 3 || numbers.length > 10) {
+      alert('Wrong amount of numbers');
+      return;
+    }
+    
+    for (var i = 0; i < numbers.length; i++) {
+      var item = numbers[i];
+      var num = parseInt(item);
+      
+      if (isNaN(num) || num > 100) {
+          alert('This should be NUMBER less than a hundred.');
+        return;
+      } 
+      numbers[i] = num;
+    }
+    numbers.sort((a,b) => a - b); 
+    console.log(numbers.join(', '));
   }
 
-  console.log(isNumeric(true));
+  home1();
