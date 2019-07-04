0600b27ab551695978eb0180cd33965081441979
Eugene Kuchma <evgnkuchma@gmail.com>
2019-07-04 23:55:41 +0300

    home 6 done

--- home6/part2/js/script.js
+++ home6/part2/js/script.js
@@ -1,70 +1,57 @@
 "use strict"
 
-//реализация циклом
-
-// function showEven(n=prompt("enter the value")) { 
-//     if (n<0){
-//         console.log("n must be greater than zero ");
-//     } else if (isNaN(n)){
-//         console.log("n must be a numeric value");
-//     }
-//     for (var i = 0; i <= n; i++) {
-//         if(i%2==0){
-//             console.log(i);
-//         }
-//     }
-// }
-
-// showEven();
-
-//реализация рекурсией
-
-// var n=prompt("enter the value");
-// function showEven(n) { 
-//     if (n<0){
-//         console.log("n must be greater than zero ");
-//     } else if (isNaN(n)){
-//         console.log("n must be a numeric value");
-//     }
-//     if (0 == n){
-//         console.log(n);
-//         return
-//     } else if (n %2 == 0) {
-//         console.log((n));
-//         showEven(--n);
-//     } else if (n %2 == 1) {
-//         showEven(--n);
-//     }     
-// }
-// showEven(n);
-
-
-
-//part2.1
-
-function cons(){
-    console.log('c is a function')
-}
-
-function f(a, b, c) {
-    {
-        if (a === undefined) {
-            a = 2;
-        }
-        if (b === undefined) {
-            b = 3;
-        }
-        function sum(a, b){
-            return a + b;
-        }
-
-         if (typeof(c) =="function") {
-            return c();
-        } else {
-            return sum(a,b)
-        }
-
-
+var obj1 = {
+    a: 3,
+    b: 4,
+    c: {
+       a: {
+          a: 38,
+          b: 21
+       },
+       b: 6
     }
-}
-console.log(f(2,3,cons));
+ };
+ 
+ 
+ var obj2 = {
+    a: 3,
+    b: 58,
+    d: 32,
+    c: {
+       a: {
+          a: 2,
+          b: 1
+       }
+    }
+ };
+
+ var result = {}
+ function mergeObj (){
+     for(var key in obj1) {
+         if(typeof obj1[key] === "object" && obj1[key] !== null){
+             var currentObj = obj1[key];
+             result[key] = {};
+
+             for(var subKey in currentObj){
+                 result[key][subKey] = currentObj[subKey];
+             }
+         } else {
+             result[key] = obj1[key]
+         }
+     }
+     
+     for(var key in obj2) {
+        if(typeof obj2[key] === "object" && obj2[key] !== null){
+            var currentObj = obj2[key];
+
+            for(var subKey in currentObj){
+                result[key][subKey] = currentObj[subKey];
+            }
+        } else {
+            result[key] = obj2[key]
+        }
+    }
+    console.log(result);
+ }
+
+ mergeObj()
