4e2e1aaef2bebae7930aab2d2c9307db163db921
Eugene <evgnkuchma.gmail.com>
2019-07-12 14:46:12 +0300

    added 9 homework

--- home6/part2/js/script.js
+++ home6/part2/js/script.js
@@ -4,47 +4,48 @@ var obj1 = {
     a: 3,
     b: 4,
     c: {
-       a: {
-          a: 38,
-          b: 21
-       },
-       b: 6
+        a: {
+            a: 38,
+            b: 21
+        },
+        b: 6
     }
- };
- 
- 
- var obj2 = {
+};
+
+
+var obj2 = {
     a: 3,
     b: 58,
     d: 32,
     c: {
-       a: {
-          a: 2,
-          b: 1
-       }
+        a: {
+            a: 2,
+            b: 1
+        }
     }
- };
+};
 
- var result = {}
- function mergeObj (){
-     for(var key in obj1) {
-         if(typeof obj1[key] === "object" && obj1[key] !== null){
-             var currentObj = obj1[key];
-             result[key] = {};
+var result = {}
 
-             for(var subKey in currentObj){
-                 result[key][subKey] = currentObj[subKey];
-             }
-         } else {
-             result[key] = obj1[key]
-         }
-     }
-     
-     for(var key in obj2) {
-        if(typeof obj2[key] === "object" && obj2[key] !== null){
+function mergeObj() {
+    for (var key in obj1) {
+        if (typeof obj1[key] === "object" && obj1[key] !== null) {
+            var currentObj = obj1[key];
+            result[key] = {};
+
+            for (var subKey in currentObj) {
+                result[key][subKey] = currentObj[subKey];
+            }
+        } else {
+            result[key] = obj1[key]
+        }
+    }
+
+    for (var key in obj2) {
+        if (typeof obj2[key] === "object" && obj2[key] !== null) {
             var currentObj = obj2[key];
 
-            for(var subKey in currentObj){
+            for (var subKey in currentObj) {
                 result[key][subKey] = currentObj[subKey];
             }
         } else {
@@ -52,6 +53,6 @@ var obj1 = {
         }
     }
     console.log(result);
- }
+}
 
- mergeObj()
+mergeObj()
---
+++ lesson1/js/part1.js
@@ -0,0 +1,8 @@
+"use strict"
+
+var gummy = document.querySelector("#gummy");
+var infoText = document.querySelector("#info")
+
+gummy.addEventListener("click", function(event){
+    console.log("gummy")
+});
