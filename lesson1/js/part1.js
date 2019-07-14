66afdd5653c4109d9c892241e6b712edaa852695
Eugene <evgnkuchma.gmail.com>
2019-07-14 23:42:50 +0300

    some changes

--- lesson1/js/part1.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-var gummy = document.querySelector("#gummy");
-var infoText = document.querySelector("#info")
-
-gummy.addEventListener("click", function(event){
-    console.log("gummy")
-});
